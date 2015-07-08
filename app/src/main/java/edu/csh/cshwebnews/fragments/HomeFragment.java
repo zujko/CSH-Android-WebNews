a83ad1ba9a97fcf3d04b6eeabdb35ec531161356
z-ko <zujko@csh.rit.edu>
2015-07-08 12:31:44 -0400

    Created initial home fragment

---
+++ app/src/main/java/edu/csh/cshwebnews/fragments/HomeFragment.java
@@ -0,0 +1,37 @@
+package edu.csh.cshwebnews.fragments;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
