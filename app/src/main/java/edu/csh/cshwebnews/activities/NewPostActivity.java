a7822c9dfbf61d4b8f4ffb2a8f9c5676d59c80d3
z-ko <zujko@csh.rit.edu>
2015-07-13 10:36:45 -0400

    Created a spinner which shows all newsgroups the user can post to

--- app/src/main/java/edu/csh/cshwebnews/activities/NewPostActivity.java
+++ app/src/main/java/edu/csh/cshwebnews/activities/NewPostActivity.java
@@ -1,24 +1,51 @@
 package edu.csh.cshwebnews.activities;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import edu.csh.cshwebnews.R;
+import edu.csh.cshwebnews.adapters.NewsgroupSpinnerAdapter;
+import edu.csh.cshwebnews.database.WebNewsContract;
 
-public class NewPostActivity extends AppCompatActivity {
+public class NewPostActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
+    private static final int NEWSGROUP_LOADER = 0;
     private Toolbar mToolbar;
+    private Spinner mSpinner;
+    private NewsgroupSpinnerAdapter mSpinnerAdapter;
+    private ImageView mDownArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
         setUpToolbar();
+
+        mSpinnerAdapter = new NewsgroupSpinnerAdapter(this,null,0);
+        mSpinner = (Spinner) findViewById(R.id.spinner);
+        mSpinner.setAdapter(mSpinnerAdapter);
+
+        mDownArrow = (ImageView) findViewById(R.id.down_arrow_image);
+        mDownArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSpinner.performClick();
+            }
+        });
+
+        getSupportLoaderManager().initLoader(NEWSGROUP_LOADER, null, this);
     }
 
     @Override
@@ -64,5 +91,28 @@ public class NewPostActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle(getString(R.string.activity_new_post_title));
 
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String sortOrder = WebNewsContract.NewsGroupEntry.NAME;
+        Uri newsgroupUri = WebNewsContract.NewsGroupEntry.CONTENT_URI;
+
+        return new CursorLoader(this,
+                newsgroupUri,
+                WebNewsContract.NEWSGROUP_COLUMNS,
+                WebNewsContract.NewsGroupEntry.POSTING_ALLOWED + " = ?",
+                new String[]{"1"},
+                sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mSpinnerAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mSpinnerAdapter.swapCursor(null);
     }
 }
