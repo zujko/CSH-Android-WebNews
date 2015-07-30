59855e747f11add99ca4ce229628bc26c01c48cc
z-ko <zujko@csh.rit.edu>
2015-07-30 11:21:16 -0400

    Created blank fragment

---
+++ app/src/main/java/edu/csh/cshwebnews/fragments/PostFragment.java
@@ -0,0 +1,27 @@
+package edu.csh.cshwebnews.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.csh.cshwebnews.R;
+
+
+public class PostFragment extends Fragment {
+
+
+    public PostFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+
+}
