9a3777c441a2047e8b320f692187b16a27d1aca9
Peter Zujko <zujko@csh.rit.edu>
2015-09-07 00:09:36 -0400

    Deleted child layout and changed expandable listview to a plain listview

--- app/src/main/java/edu/csh/cshwebnews/fragments/PostFragment.java
+++ app/src/main/java/edu/csh/cshwebnews/fragments/PostFragment.java
@@ -6,19 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import edu.csh.cshwebnews.R;
 
 
 public class PostFragment extends Fragment {
 
+    @Bind(R.id.post_list) ListView mPostListView;
 
     public PostFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_post,container,false);
+        ButterKnife.bind(this,rootView);
 
-        return inflater.inflate(R.layout.fragment_post, container, false);
+
+        return rootView;
     }
 
 
